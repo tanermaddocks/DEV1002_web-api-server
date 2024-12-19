75bdab277fdf4fd47498ad8766afc0a5410a54a3
tanermaddocks <maddockstaner@gmail.com>
2024-12-19 10:41:26 +1000

    Renamed import_controllers

---
+++ controllers/controllers_import.py
@@ -0,0 +1,8 @@
+# Reroute for imports to make main.py cleaner
+
+from booking_controller import *
+from booking_table_controller import *
+from cli_controller import *
+from guest_controller import *
+from controllers.venue_controller import *
+from table_controller import *
