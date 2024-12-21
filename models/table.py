d51895d83f85b08e89cae96f526c985c36a5acaa
tanermaddocks <maddockstaner@gmail.com>
2024-12-21 10:48:52 +1000

    Updated max_guests value to be an integer

--- models/table.py
+++ models/table.py
@@ -10,7 +10,7 @@ class Table(db.Model):
     # Columns
     id = db.Column(db.Integer, primary_key=True)
     table_number = db.Column(db.Integer, nullable=False) #add in parameter for table#/venueid
-    max_guests = db.Column(db.String, nullable=False)
+    max_guests = db.Column(db.Integer, nullable=False)
     venue_id = db.Column(db.Integer, db.ForeignKey("venues.id"), nullable=False)
 
     # Relationships
