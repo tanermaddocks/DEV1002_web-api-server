0f35d1f42bcfa514211b4bb5546c4270dfb1992b
tanermaddocks <maddockstaner@gmail.com>
2024-12-22 10:08:06 +1000

    Uncommented error handlers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from marshmallow import ValidationError
 
 from local_import.init import db, ma
 from local_import.controllers import *
@@ -13,17 +14,17 @@ def create_app():
     db.init_app(app)
     ma.init_app(app)
 
-    # @app.errorhandler(ValidationError)
-    # def validation_error(err):
-    #     return {"message": err.messages}, 400
+    @app.errorhandler(ValidationError)
+    def validation_error(err):
+        return {"message": err.messages}, 400
     
-    # @app.errorhandler(400)
-    # def bad_request(err):
-    #     return {"message": str(err)}, 400
+    @app.errorhandler(400)
+    def bad_request(err):
+        return {"message": str(err)}, 400
     
-    # @app.errorhandler(404)
-    # def not_found(err):
-    #     return {"message": str(err)}, 404
+    @app.errorhandler(404)
+    def not_found(err):
+        return {"message": str(err)}, 404
     
     app.register_blueprint(db_commands)
     app.register_blueprint(venue_bp)
