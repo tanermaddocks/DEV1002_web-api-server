c2f729707e409b21908076179ecbfb9636bda92e
tanermaddocks <maddockstaner@gmail.com>
2024-12-22 10:46:37 +1000

    Added validation for name and phone

--- models/venue.py
+++ models/venue.py
@@ -1,4 +1,5 @@
 from marshmallow import fields
+from marshmallow.validate import Regexp, And, Length
 
 from local_import.init import db, ma
 
@@ -17,6 +18,16 @@ class Venue(db.Model):
 
 # Schema
 class VenueSchema(ma.Schema):
+    # Validations
+    name = fields.String(validate=And(
+        Length(min=2, error="Name must be at least 2 characters long"),
+        Regexp('^[A-Za-z][A-Za-z0-9 ]*$',
+               error="Only letters, numbers and spaces are allowed")
+               ))
+    phone = fields.String(
+        validate=Regexp("(?:\+?61)?(?:\(0\)[23478]|\(?0?[23478]\)?)\d{8}",
+                        error="Input a valid Australian phone number"
+                        ))
     # Modifiers
     tables = fields.Nested("TablesSchema", exclude=["venue"])
     # Fields
